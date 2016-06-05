daa8f348daa1a7411ad91d9ee56cb4809e9d84ed
zkske121 <zkske121@163.com>
2016-06-05 12:02:29 +0800

    首页说明

--- demo/redux-react-director3/components/Home.js
+++ demo/redux-react-director3/components/Home.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 
 export default class Home extends Component {
 	render() {
-		return <div>Home</div>;
+		return (
+			<div style={{textAlign:'center'}}>
+				<h2 style={{margin:'50px 0'}}>一个简单的使用redux-react实现的demo</h2>
+				<h3>路由是由redux管理，使用<a href='https://github.com/flatiron/director' target='_blank'>director.js</a>实现</h3>
+				<h3>异步请求使用redux-thunk方式</h3>
+				<h3>组件使用第三方<a href='http://ant.design' target='_blank'>ant.design</a></h3>
+				</div>
+			);
 	}
 }
--- demo/redux-react-director3/containers/App.js
+++ demo/redux-react-director3/containers/App.js
@@ -78,7 +78,8 @@ class App extends Component {
               </Col>
             </Row>
             <footer className='footer'>
-              <h2>此demo仅供个人学习使用</h2>
+              <h2>此demo仅供个人学习使用  <a target='_blank' href='https://github.com/zkske121/daily/tree/master/demo/redux-react-director3'>源码</a>
+              </h2>
             </footer>
         </div>
       )
